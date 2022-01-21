54b0c2eb351eac8603e63a1179014fbb617a9ef3
alber <alber@10.100.141.252>
2022-01-21 12:02:31 +0100

    entrega

--- src/main/java/org/springframework/samples/petclinic/care/Care.java
+++ src/main/java/org/springframework/samples/petclinic/care/Care.java
@@ -2,6 +2,17 @@ package org.springframework.samples.petclinic.care;
 
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.PetType;
 
 import lombok.Getter;
@@ -9,8 +20,24 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class Care {
-    String name;
+@Entity
+public class Care extends BaseEntity{
+	@NotEmpty
+	@Size(min=5,max=30)
+	@Column(name="name", unique=true)
+	String name;
+	@NotEmpty
     String description;
+	@NotNull
+	@NotEmpty
+	@ManyToMany(cascade = {CascadeType.ALL})
+	@JoinTable(name = "compatible_pet_types", joinColumns = @JoinColumn(name = "pet_type_id"),
+	inverseJoinColumns = @JoinColumn(name = "care_id"))
     Set<PetType> compatiblePetTypes;
+	@ManyToMany(cascade = {CascadeType.ALL})
+	@JoinTable(name = "incompatible_cares", joinColumns = @JoinColumn(name = "care_id1"),
+	inverseJoinColumns = @JoinColumn(name = "care_id2"))
+	Set<Care> incompatiblesCares;
+	
+	
 }
